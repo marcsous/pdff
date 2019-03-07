098c9c1d8859b5094f8f00096bf7327b01deccca
marcsous <17042435+marcsous@users.noreply.github.com>
2019-03-07 11:09:43 -0800

    Add files via upload

---
+++ ndb_example.m
@@ -0,0 +1,26 @@
+%% fit phantom data from the paper
+%
+% https://doi.org/10.1016/j.mri.2011.07.004
+%
+%load PHANTOM_NDB_PAPER.mat
+load spine_ucla.mat
+
+H2O = 4.7;
+data = squeeze(data(:,:,3,:));
+
+[nx ny ne] = size(data);
+params = zeros(nx,ny,5);
+
+% loop over x and y dimensions
+for x = 1:nx
+    
+    ims(params,[],{'B0 (Hz)','R2* (1/s)','FF (%)','PHI (rad)','NDB'}); drawnow
+    
+    for y = 1:ny
+        
+        if any(data(x,y,:))
+            params(x,y,:) = struct2array(ndbest(te,data(x,y,:),Tesla,H2O));
+        end
+        
+    end
+end
