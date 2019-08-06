40a526bc6f70cffacfa3ae2c6a298c69cf092e43
marcsous <17042435+marcsous@users.noreply.github.com>
2019-08-06 14:32:24 -0700

    Add files via upload

--- lipoquant.m
+++ lipoquant.m
@@ -2,13 +2,13 @@ function [params sse] = lipoquant(te,data,Tesla,varargin)
 %[params sse] = lipoquant(te,data,Tesla,varargin)
 %
 % Proton density fat fraction estimation using magnitude.
-% Returns struct of FF and R2*.
+% Returns struct of FF (%) and R2* (1/s).
 %
 % Inputs:
 %  te is echo time in seconds (vector)
 %  data (1D, 2D or 3D with echos in last dimension)
 %  Tesla field strength (scalar)
-%  varargin in option/value pairs (e.g. 'init',FF)
+%  varargin in option/value pairs (e.g. 'init',0.1)
 %
 % Alternate input:
 %  imDataParams is a struct from fat-water toolbox
@@ -21,17 +21,18 @@ function [params sse] = lipoquant(te,data,Tesla,varargin)
 % demo code
 if nargin==0
     load phantom_16echo_bipolar.mat
-    data = abs(data);
+    %load PHANTOM_NDB_PAPER.mat
+    %load liver_gre_3d_2x3.mat; data = data(:,:,31,:);
 end
 
 %% default options
 
 opts.ndb = 2.5; % no. double bonds
-opts.h2o = 4.7; % water frequency ppm
+opts.h2o = 4.7; % water ppm
 opts.maxit = 20; % max. no. iterations
-opts.softplus = 1; % nonnegative R2*
-opts.noise = []; % noise std (if available)
-opts.init = []; % initial FF estimate
+opts.nonnegR2 = 1; % nonnegative R2*
+opts.noise = 0; % noise std (if available)
+opts.init = 0.1; % initial FF estimate (0-1)
 
 % varargin handling (must be option/value pairs)
 for k = 1:2:numel(varargin)
@@ -82,6 +83,7 @@ if ~isreal(data) || nnz(data<0)
     warning('data should be magnitude. Converting to magnitude.');
     data = abs(data);
 end
+[nx ny nz ne] = size(data);
 
 %% see if gpu is possible
 
@@ -95,68 +97,44 @@ catch ME
     warning('%s. Using CPU.',ME.message);
 end
 
-%% calculate constants
-
-% estimate noise std (effective but heavy)
-if isempty(opts.noise)
-    X(:,9) = reshape(circshift(data,[0 0]),[],1);
-    X(:,8) = reshape(circshift(data,[0 1]),[],1);
-    X(:,7) = reshape(circshift(data,[1 0]),[],1);
-    X(:,6) = reshape(circshift(data,[1 1]),[],1);
-    X(:,5) = reshape(circshift(data,[2 0]),[],1);
-    X(:,4) = reshape(circshift(data,[0 2]),[],1);
-    X(:,3) = reshape(circshift(data,[2 1]),[],1);
-    X(:,2) = reshape(circshift(data,[1 2]),[],1);
-    X(:,1) = reshape(circshift(data,[2 2]),[],1);
-    X = min(svd(X'*X)); % overwrite X (memory)
-    opts.noise = gather(sqrt(X/nnz(data)));
-end
-
-% time evolution matrix
-te = cast(te,'like',opts.noise);
-opts.A = fat_basis(te,Tesla,opts.ndb,opts.h2o);
-
 % display
 disp([' Data size: [' sprintf('%i ',size(data)) sprintf('\b]')])
 disp([' TE (ms): ' sprintf('%.2f ',1000*te(:))])
 disp([' Tesla (T): ' sprintf('%.2f',Tesla)])
 disp(opts);
 
-[nx ny nz ne] = size(data);
+%% faster calculations
 
-%% echos in 1st dimension (faster dot products)
-
+% echos in 1st dimension
 data = permute(data,[4 1 2 3]);
+
+% consistent data types
 te = reshape(real(cast(te,'like',data)),ne,1);
 
 %% nonlinear estimation (local optmization)
 
-% initial estimates (doesn't help being clever with these)
-if isempty(opts.init)
-    opts.init = 0; % fat fraction
-else
-    if ~isreal(opts.init) || nnz(opts.init<0 | opts.init>1)
-        warning('opts.init values should be [0 1].');
-    end
-    opts.init = min(max(real(opts.init),0),1);
+% initial estimates
+if ~isreal(opts.init) || nnz(opts.init<0 | opts.init>1)
+    warning('opts.init values should be [0 1].');
 end
+opts.init = min(max(real(opts.init),0),1);
 
-M = 1.25*reshape(max(data)/opts.A(1,1),1,nx*ny*nz);
 if isscalar(opts.init)
     F = repmat(cast(opts.init,'like',data),1,nx*ny*nz);
 else
     F = reshape(cast(opts.init,'like',data),1,nx*ny*nz);
 end
-
+M = reshape(max(data),1,nx*ny*nz);
 R = repmat(cast(0,'like',data),1,nx*ny*nz);
 
 % find the local minimum
-[M F R sse] = nlsfit(M,F,R,te,data,opts);
-
-% nonegative R2*
-if opts.softplus; R = softplus(R); end
+[sse M F R] = nlsfit(M,F,R,te,data,Tesla,opts);
 
 %% return parameters in correct format
+
+% nonegative M and R2*
+M = abs(M);
+if opts.nonnegR2; R = abs(R); end
 
 M = reshape(M,nx,ny,nz);
 F = reshape(F,nx,ny,nz);
@@ -167,14 +145,15 @@ params.FF =  gather(100*F); % FF (%)
 params.R2 = gather(R); % R2* (1/s)
 
 sse = gather(sse);
+fprintf(' sse = %.6e\n',sqrt(sum(sse(:))));
 
 %% nonlinear least squares fitting
-function [M F R sse] = nlsfit(M,F,R,te,data,opts)
+function [sse M F R] = nlsfit(M,F,R,te,data,Tesla,opts)
 
 [ne nx ny nz] = size(data);
 
-% Levenberg Marquardt parameter
-lambda = repmat(opts.noise^2/1000,size(M));
+% water-fat matrix
+opts.A = fat_basis(te,Tesla,opts.ndb,opts.h2o);
 
 % residual and Jacobian
 [r JM JF JR] = myfun(M,F,R,te,data,opts);
@@ -188,74 +167,84 @@ for iter = 1:opts.maxit
     % | a31 a32 a33 |             |   a32.a21-a31.a22 -(a32.a11-a31.a12)  a22.a11-a21.a12  |
     %
     % with dtm = a11.(a33.a22-a32.a23)-a21.(a33.a12-a32.a13)+a31.(a23.a12-a22.a13)
-    
-    % J'*J+mu.I
-    a11 = dot(JM,JM)+lambda;
+
+    a11 = dot(JM,JM);
     a12 = dot(JM,JF);
     a13 = dot(JM,JR);
     a21 = a12;
-    a22 = dot(JF,JF)+lambda;
+    a22 = dot(JF,JF);
     a23 = dot(JF,JR);
     a31 = a13;
     a32 = a23;
-    a33 = dot(JR,JR)+lambda;
-    dtm = a11.*(a33.*a22-a32.*a23)-a21.*(a33.*a12-a32.*a13)+a31.*(a23.*a12-a22.*a13);
+    a33 = dot(JR,JR);
 
-    % J'*r
-    JMr = dot(JM,r);
-    JFr = dot(JF,r);
-    JRr = dot(JR,r);
+    % Levenberg Marquardt term
+    if iter==1; lambda = (a11+a22+a33+mean(data(:)))/1000; end
+ 
+    a11 = a11 + lambda;
+    a22 = a22 + lambda;
+    a33 = a33 + lambda;
+
+    dtm = a11.*(a33.*a22-a32.*a23)-a21.*(a33.*a12-a32.*a13)+a31.*(a23.*a12-a22.*a13);
     
-    % updates = -inv(J'J+mu.I)*J'r
-    dM =(-(a33.*a22-a32.*a23).*JMr+(a33.*a12-a32.*a13).*JFr-(a23.*a12-a22.*a13).*JRr)./dtm; 
-    dF =(+(a33.*a21-a31.*a23).*JMr-(a33.*a11-a31.*a13).*JFr+(a23.*a11-a21.*a13).*JRr)./dtm;
-    dR =(-(a32.*a21-a31.*a22).*JMr+(a32.*a11-a31.*a12).*JFr-(a22.*a11-a21.*a12).*JRr)./dtm;
+    % gradient (J'*r)
+    gM = dot(JM,r);
+    gF = dot(JF,r);
+    gR = dot(JR,r);
+
+    % updates = -inv(J'J+lambda.I)*J'r
+    dM =(-(a33.*a22-a32.*a23).*gM+(a33.*a12-a32.*a13).*gF-(a23.*a12-a22.*a13).*gR)./dtm; 
+    dF =(+(a33.*a21-a31.*a23).*gM-(a33.*a11-a31.*a13).*gF+(a23.*a11-a21.*a13).*gR)./dtm;
+    dR =(-(a32.*a21-a31.*a22).*gM+(a32.*a11-a31.*a12).*gF-(a22.*a11-a21.*a12).*gR)./dtm;
     
     % evaluate at new estimate
     [r_new JM_new JF_new JR_new] = myfun(M+dM,F+dF,R+dR,te,data,opts);
-    
-    % accept changes that gave an improvement    
+
+    % accept changes that gave an improvement
     ok = dot(r_new,r_new) < dot(r,r);
     r(:,ok) = r_new(:,ok);
     M(ok) = M(ok)+dM(ok);
     F(ok) = F(ok)+dF(ok);
     R(ok) = R(ok)+dR(ok);
     JM(:,ok) = JM_new(:,ok);
-    JF(:,ok) = JF_new(:,ok); 
+    JF(:,ok) = JF_new(:,ok);
     JR(:,ok) = JR_new(:,ok);
-    
-    % update Levenberg Marquardt parameter
+   
+    % update damping term
     lambda(ok) = lambda(ok) / 10;
     lambda(~ok) = lambda(~ok) * 9;
 
-    % error norm
-    sse = sum(r.^2);
-
 end
+
+% error norm
+sse = sum(r.^2);
 
 % display maps
 if numel(data)==ne
-    plot(1e3*te,data,'o');hold on;plot(1e3*te,data+r);hold off;xlabel('te (ms)');
-    title(['||r||=' num2str(sqrt(sse)) ' FF=' num2str(100*F,'%.1f')]);
-    fprintf('R2=%.3f\tFF=%.3f\tsse=%.5e\n',R,100*F,sse);pause(0.1);
+    plot(1e3*te,data,'o'); temp = linspace(min(te),max(te),100*numel(te)).';
+    opts.A = fat_basis(temp,Tesla,opts.ndb,opts.h2o); xlabel('te (ms)');
+    hold on; plot(1e3*temp,myfun(M,F,R,temp,data,opts)); hold off
+    if opts.nonnegR2; tmp = abs(R); else tmp = R; end
+    title(['FF=' num2str(100*F,'%.1f%%') '  R2=' num2str(tmp,'%.1f') '  ||r||=' num2str(sqrt(sse))]);
+    fprintf('R2=%.3f\tFF=%.3f\tsse=%.5e\n',tmp,F,sse);pause(0.1);
 else
     mid = ceil(nz/2);
     subplot(1,2,1); tmp = reshape(F,nx,ny,nz);
-    imagesc(100*tmp(:,:,mid),[-5 105]); colorbar; title('PDFF (%)');
+    imagesc(100*tmp(:,:,mid),[-5 55]); colorbar; title('PDFF (%)');
     subplot(1,2,2); tmp = reshape(R,nx,ny,nz);
-    if opts.softplus; tmp = softplus(tmp); end
-    imagesc(tmp(:,:,mid),[0 200]); colorbar; title('R2* (s^{-1})'); drawnow
+    if opts.nonnegR2; tmp = abs(tmp); end
+    imagesc(tmp(:,:,mid),[0 155]); colorbar; title('R2* (s^{-1})'); drawnow
 end
-
 
 %% model function
 function [f JM JF JR] = myfun(M,F,R,te,data,opts)
-
+  
 % no. echos
 ne = numel(te);
 
-% nonnegative R2*
-if opts.softplus; [R dR] = softplus(R); end
+% nonnegative M and R2*
+M = abs(M);
+if opts.nonnegR2; R = abs(R); end
 
 % components of the signal
 fat = opts.A(:,2)*F;
@@ -273,26 +262,26 @@ if nargout>1
    
     JM = s1.*s2.^2.*s3.^2./f;
     JR = bsxfun(@times,-te,JM.*s1);
-    if opts.softplus; JR = JR.*dR; end
     
-    % finite difference (analytical too hard)
-    h = 1e-4; % 0<F<0.5 so h=1e-4 is ok
-    s4 = bsxfun(@times,diff(opts.A,1,2),h);
-    s2 = abs(water+fat+s4);
-    JF = (hypot(s1.*s2.*s3,opts.noise)-f)/h;
-    
+    if opts.nonnegR2
+        signR = sign(R)+(R==0);
+        JR = JR.*signR;
+    end
+
+    % JF (analytical)    
+    tmp = abs(opts.A(:,2)).^2-real(opts.A(:,1)).^2-2*real(prod(opts.A,2));
+    tmp = bsxfun(@plus,diff(real(opts.A),1,2),tmp*F);
+    JF = s1.^2.*s3.^2.*tmp./f;  
+
+    % JF (finite difference)
+    %h = 1e-4;
+    %s4 = bsxfun(@times,diff(opts.A,1,2),h);
+    %s2 = abs(water+fat+s4);
+    %JF = (hypot(s1.*s2.*s3,opts.noise)-f)./h;
+
 end
 
 % residual (or display)
 if numel(f)==numel(data)
     f = f-reshape(data,size(f));
 end
-
-%% softplus function
-function [R dR] = softplus(R)
-ok = R < log(flintmax('single'));
-expR = exp(double(R(ok)));
-expRp1 = expR+1;
-R(ok) = log(expRp1);
-dR = ones(size(R),'like',R);
-dR(ok) = expR./expRp1;
