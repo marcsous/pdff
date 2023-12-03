5e18b78204ffafab17729ae008192eef2d545712
Mark Bydder <17042435+marcsous@users.noreply.github.com>
2023-12-03 15:03:51 -0800

    Add files via upload

--- grad_delay_corr.m
+++ grad_delay_corr.m
@@ -8,6 +8,10 @@ function [data phi] = grad_delay_corr(data,dim)
 %
 % -data: 2D or 3D complex multi-echo images (echos in last dim)
 % -dim: the readout dimension (default 2)
+%
+% Refs.
+% doi.org/10.1016/j.mri.2006.03.006
+% doi.org/10.1016/j.mri.2022.08.017
 
 % demo dataset
 if nargin==0
@@ -26,6 +30,23 @@ elseif ~ismember(dim,[1 2 3])
     error('dim is not supported');
 end
 
+% minimize cost function
+opts = optimset('Display','off','GradObj','on');
+[phi,~,~,~,~,H] = fminunc(@(phi)myfun(phi,data,dim),0,opts);
+
+% 95% confidence interval
+ci95 = sqrt(diag(inv(abs(H)))) * 1.96;
+
+for k = 1:numel(phi)
+    fprintf('Gradient delay = %f ± %f dwell times\n',phi(k),ci95(k));
+end
+
+% get corrected data
+[~,~,data] = myfun(phi,data,dim);
+
+%% cost function: nuclear norm of A
+function [nrm grd data] = myfun(phi,data,dim)
+
 % phase roll along readout (unit: 2pi phase cycle <=> 1 kspace point <=> 1 dwell time)
 roll = i * linspace(-pi,pi,size(data,dim));
 if dim==1; roll = reshape(roll,[],1,1); end
@@ -35,43 +56,32 @@ if dim==3; roll = reshape(roll,1,1,[]); end
 % phase matrix
 s = size(data); s(dim) = 1;
 P = repmat(cast(roll,'like',data),s);
+
+% alternate on odd/even echos
 if ndims(data)==3; P(:,:,  2:2:end) = -P(:,:,  2:2:end); end
 if ndims(data)==4; P(:,:,:,2:2:end) = -P(:,:,:,2:2:end); end
-
-% minimize cost function
-opts = optimset('Display','off','GradObj','on');
-[phi,~,~,~,~,H] = fminunc(@(phi)myfun(phi,data,P),0,opts);
-
-% 95% confidence interval
-ci95 = sqrt(diag(inv(abs(H)))) * 1.96;
-
-fprintf('Gradient delay = %f ± %f dwell times\n',phi,ci95);
-
-% get corrected data
-[~,~,data] = myfun(phi,data,P);
-
-%% cost function: nuclear norm of A
-function [nrm grd data] = myfun(phi,data,P)
 
 % phase correct data
 data = exp(phi*P) .* data;
 
-% matrix of echo variation in all pixels (doi.org/10.1016/j.mri.2006.03.006)
-A = reshape(data,[],size(data,ndims(data)));
+% matrix of echo variation in all pixels
+A = reshape(data,[],s(end));
 
-% nuclear norm and derivative w.r.t. phi (doi.org/10.1016/j.mri.2022.08.017)
+% nuclear norm and derivative w.r.t. phi
 if nargout==1
     
     W = svd(A);
+    dW = [];
     
 else
     
-    [~,W,V] = svd(A,'econ');
-    W = diag(W);
+    [U W V] = svd(A,'econ');
+	W = diag(W);
     dA = A.*reshape(P,size(A));
-    dW = real(diag(V'*(A'*dA)*V))./W;
-    grd = gather(sum(dW,'double'));
+    dW = real(diag((U'*dA)*V));
 
 end
 
-nrm = gather(sum(W,'double'));
+% plain doubles for fminunc
+nrm = gather(sum( W,'double'));
+grd = gather(sum(dW,'double'));
